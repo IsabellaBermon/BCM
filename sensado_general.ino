7bef2b2d8d93d2579e95a395ce97ce72fa61ee4b
IsabellaBermon <116314660+IsabellaBermon@users.noreply.github.com>
2023-03-20 17:17:40 -0500

    add load cell measurement

--- sensado_general.ino
+++ sensado_general.ino
@@ -1,25 +1,47 @@
+#include "HX711.h"
+
 // Current sensor var
 uint32_t actual_current, avg_current, sens_current, sample_counter = 0;
 uint32_t offset_i = 0;
+
 // Thurst sensor var
 uint32_t actual_thrust, avg_thrust = 0;
+uint32_t scale_cell = 439430.25; // valor incial antes de calibrar / valor esperado antes de calibrar de objeto conocido
+
 // Temperature sensor var
 uint32_t actual_temp, avg_temp = 0;
 uint32_t offset_temp = 0;
+
 // Velocity sensor var
 uint32_t actual_vel, avg_vel = 0;
-// Default var
+
+// Default variables
 uint32_t period = 500; //ms
 uint32_t fs = 200; //Hz
 #define sample_time 1/fs ; //5ms
 uint32_t n_samples = 1;
 uint32_t max_current = 1; //A
+
 // Set clocks
 uint32_t currentTime = 0; 
 bool start = true;
 
+// Pins
+int acs = A0;
+int lm35 = A1;
+int DOUT_loadcell = A2;
+int CLK_loadcell = A3;
+
+HX711 m_thrust;
+
 void setup() {
   noInterrupts();
+  pinMode(acs,INPUT);
+  pinMode(lm35,INPUT);
+  m_thrust.begin(DOUT_loadcell, CLK_loadcell);
+  m_thrust.set_scale(scale_cell); // scale 
+  m_thrust.tare(20);  //El peso actual es considerado Tara.
+  // timer counter setup
   TCCR1A = 0;
   TCCR1B = 0;
   TCCR1B |= B00000100; //Set prescalar to 256
@@ -70,13 +92,13 @@ void update_measurement(uint32_t sample_counter){
   }
   
 uint32_t sense_current(){
-  uint32_t voltaje = analogRead(A0) * (5.0 / 1023.0);
+  uint32_t voltaje = analogRead(acs) * (5.0 / 1023.0);
   uint32_t current = offset_i +(voltaje -2.5)/sens_current; // calculate current 
   return current
 }
 
 uint32_t sense_temperature(){
-  uint32_t voltage = analogRead(A1) * (5.0 / 1024.0);
+  uint32_t voltage = analogRead(lm35) * (5.0 / 1023.0);
   uint32_t temp = offset_temp + voltage * 100;
   return temp
 }
@@ -88,7 +110,7 @@ uint32_t sense_vel(){
 }
 
 uint32_t sense_thrust(){
-  uint32_t thrust = analogRead(A3);
+  uint32_t thrust = m_thrust.get_units(20);
   return thrust
 }
 
